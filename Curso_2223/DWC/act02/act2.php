4282fa46166ba54d8108f80d74010727aef10473
carlos_blanco <carlosblancofranco@gmail.com>
2023-02-16 19:22:00 +0100

    16 febrero clase

---
+++ Curso_2223/DWC/act02/act2.php
@@ -0,0 +1,42 @@
+<?php
+    // contraseña en clase 12345 pero en casa es 1234
+    $conexion = mysqli_connect('localhost', 'root', '12345');
+    mysqli_select_db($conexion, 'world');
+    $consultaCiudades = mysqli_prepare($conexion, "SELECT Name FROM city;");
+    $consultaCiudades->execute();
+    $resultado = $consultaCiudades->get_result();
+
+    $arrayCiudades = array();
+
+    while ($myrow = $resultado->fetch_assoc()) {
+
+        array_push($arrayCiudades, $myrow);
+
+    }
+
+    $nombreCiudad = strtolower($_REQUEST["ciudad"]);
+    $hint = "<tr><td><b>PAÏSOS</b></td></tr>";
+
+    if ($nombreCiudad !== "") {
+
+        $len = strlen($nombreCiudad);
+
+        foreach ($arrayCiudades as $name) {
+
+            if (stristr($nombreCiudad, substr($name['Name'], 0, $len))) {
+
+                if ($hint === "") {
+
+                    $hint = $name['Name'];
+
+                } else {
+
+                    $hint .= "<tr><td>".$name['Name']."</td></tr>";
+
+                }
+            }
+        }
+    }
+
+    echo $hint === "" ? "No hay resultados" : $hint;
+?>
